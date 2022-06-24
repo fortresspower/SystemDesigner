df2783dce0222c2533dd95ad231ec078a67b7755
Maxim Mounier <Maxim>
2022-06-24 16:26:59 -0400

    Runs both APIS and processes data from pv watts, need to add outputs

---
+++ listeners.js
@@ -0,0 +1,109 @@
+const solarDetailsChbox = document.getElementById("solar-details");
+
+//Add event listener to bound tilt input
+document.getElementById('inp-tilt').addEventListener('change', function () {
+    let el = document.getElementById('inp-tilt');
+    console.log(el.value);
+    if (el.value > 90) { el.value = 90;} 
+    else if (el.value < 0) { el.value = 0;}
+});
+
+//Add event listener to bound azimuth input
+document.getElementById('inp-azimuth').addEventListener('change', function () {
+    let el = document.getElementById('inp-azimuth');
+    if (el.value > 359) { el.value = 359;} 
+    else if (el.value < 0) { el.value = 0; }
+});
+
+//Add event listener to bound inverter quantity
+document.getElementById("inverter-quantity").addEventListener('change', function () {
+    let el = document.getElementById("inverter-quantity");
+    if (el.value < 0) { el.value = 0;}
+})
+
+//Add event listener to bound battery quantity
+document.getElementById("battery-quantity").addEventListener('change', function () {
+    let el = document.getElementById("battery-quantity");
+    if (el.value < 0) { el.value = 0;}
+})
+
+//Handle what to do when solar detilas checkbox value changes
+solarDetailsChbox.addEventListener("change", function () {
+    if (solarDetailsChbox.checked) {
+        document.getElementById('tilt-azimuth').style.display = 'inline-block';
+    } else {
+        document.getElementById('tilt-azimuth').style.display = 'none';
+    }
+})
+
+//Set up event listener for slider that updates text for solar array
+document.getElementById("solar-array-slider").addEventListener("input", function () {
+    console.log("Reached");
+    let el = document.getElementById("solar-array-slider");
+    let kWVal = Math.round(parseInt(el.value) * 0.43);
+    document.getElementById("solar-array-panels").innerHTML = el.value + " panels";
+    document.getElementById("solar-array-kws").innerHTML = kWVal.toString() + " kW";
+    updateBatterySystemText(kWVal);
+
+});
+
+//Set up event listener for eflex vs evault max drpdown to update battery type and count
+document.getElementById("battery-options").addEventListener('change', function () {
+    let el = document.getElementById("solar-array-slider");
+    let kWVal = Math.round(parseInt(el.value) * 0.43);
+    if (document.getElementById('battery-options').value == 'eVault Max') {
+        document.getElementById('battery-quantity').value = Math.ceil(document.getElementById('battery-quantity').value * 5.4 / 18.5);
+    } else {
+        document.getElementById('battery-quantity').value = Math.ceil(document.getElementById('battery-quantity').value * 18.5 / 5.4);
+    }
+    updateBatterySystemText(kWVal);
+});
+
+//Set up event listener for eflex vs evault max drpdown to update battery type and count
+document.getElementById("battery-quantity").addEventListener('change', function () {
+    let el = document.getElementById("solar-array-slider");
+    let kWVal = Math.round(parseInt(el.value) * 0.43);
+    updateBatterySystemText(kWVal);
+});
+
+//Set up event listener to update text when slider gets updated
+document.getElementById('battery-array-slider').addEventListener('input', function () {
+    let battSelection = document.getElementById('battery-options');
+    let battSlider = document.getElementById('battery-array-slider');
+    document.getElementById('battery-array-panels').innerHTML = battSlider.value.toString() + " " +  battSelection.value + "s";
+    if (battSelection.value == 'eFlex') {
+        document.getElementById('battery-array-kwhs').innerHTML = Math.round(battSlider.value * 5.4 , 1).toString() + " kWh";
+    } else if (battSelection.value == 'eVault Max') {
+        document.getElementById('battery-array-kwhs').innerHTML = Math.round(battSlider.value * 18.5, 1).toString() + " kWh";
+    }
+})
+
+//Function update text below
+function updateBatterySystemText(kWVal) {
+    let battSelection = document.getElementById('battery-options');
+    let quantity = parseInt(document.getElementById('battery-quantity').value); 
+    let minimumBatts = 0;
+    if (battSelection.value == "eFlex") {
+        while (kWVal > 5.4 * minimumBatts) {
+            minimumBatts++;
+        }
+    } else if (battSelection.value == "eVault Max") {
+        while (kWVal > 18.5 * minimumBatts) {
+            minimumBatts++;
+        }
+    } 
+    let batteryString = minimumBatts.toString();
+    if (quantity > minimumBatts) {
+        batteryString = quantity.toString();
+    }
+    //document.getElementById('battery-system-text').innerHTML = "Your Fortress Battery system needs to be comprised of: " + batteryString + " " +  battSelection.value + "s";
+    document.getElementById('battery-array-panels').innerHTML = batteryString + " " +  battSelection.value + "s";
+    //Update Slider
+    document.getElementById('battery-array-slider').value = parseInt(batteryString);
+    //Update kwh text
+    if (battSelection.value == 'eFlex') {
+        document.getElementById('battery-array-kwhs').innerHTML = Math.round(parseInt(batteryString) * 5.4 , 1).toString() + " kWh";
+    } else if (battSelection.value == 'eVault Max') {
+        document.getElementById('battery-array-kwhs').innerHTML = Math.round(parseInt(batteryString) * 18.5, 1).toString() + " kWh";
+    }
+}
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //NREL Developer API KEY
 let api_key = "WkDNJ5GuHQJROwhr64ZgH4Hxu2fc51d3FlKijtsD"
 
+//Variables store information on HTML elements in the page
 let lastDropDownVal;
 const zipCodeInp = document.getElementById("zip-code");
 const billInp = document.getElementById("monthly-bill");
@@ -27,29 +28,120 @@ buttonVal.addEventListener("click", function() {
 
 });
 
-//Add event listener to bound tilt input
-document.getElementById('inp-tilt').addEventListener('change', function () {
-    let el = document.getElementById('inp-tilt');
-    console.log(el.value);
-    if (el.value > 90) { el.value = 90;} 
-    else if (el.value < 0) { el.value = 0;}
+//Set up button listener to call solar api
+document.getElementById('solar-api').addEventListener('click', function() {
+    runPVWattsAPI();
 });
 
-//Add event listener to bound azimuth input
-document.getElementById('inp-azimuth').addEventListener('change', function () {
-    let el = document.getElementById('inp-azimuth');
-    if (el.value > 359) { el.value = 359;} 
-    else if (el.value < 0) { el.value = 0; }
+//have solar slider call api again
+document.getElementById('solar-array-slider').addEventListener('change', function() {
+    runPVWattsAPI();
 });
 
-//Handle what to do when solar detilas checkbox value changes
-solarDetailsChbox.addEventListener("change", function () {
-    if (solarDetailsChbox.checked) {
-        document.getElementById('tilt-azimuth').style.display = 'inline-block';
-    } else {
-        document.getElementById('tilt-azimuth').style.display = 'none';
+//Run API getting solar data for given location
+function runPVWattsAPI() {
+    let system_capacity = "1" 
+    let module_type = "1"
+    let losses = "12"
+    let array_type = "1"
+    let tilt = document.getElementById('inp-tilt').value.toString();
+    let azimuth = document.getElementById('inp-azimuth').value.toString();
+
+    let rootURL = "https://developer.nrel.gov/api/pvwatts/v6.json?"
+    let requiredParameters = "api_key="+api_key+"&system_capacity="+system_capacity+"&module_type="+ module_type + "&losses=" + losses + "&array_type=" + array_type + "&tilt=" + tilt + "&azimuth=" + azimuth + "&address=" + zipCodeInp.value + "&timeframe=hourly"
+    try {
+        fetch(rootURL + requiredParameters)
+        .then(response => response.json())
+        .then(data => handlePVWattsOutput(data));
+    } catch(e) {
+        console.log(e);
     }
-})
+}
+
+
+//Function used to process PV Watts return data
+function handlePVWattsOutput(data) {
+    let systemData = {
+        pvOutput: 0,
+        solarOutflow: 0,
+        amountStored: 0,
+        gridOutflow: 0,
+        genOutput: 0,
+        genDays: 0
+    };
+    //let averageConsumption = new Array(8760);
+    //let pvOutput = new Array(8760);
+    let dcData = data.outputs.dc;
+    let solarArraySize = document.getElementById('solar-array-slider').value;
+    let averageConsumption = document.getElementById('residential-rate-inp').value / 730; //Your average monthly consumption divided by hours in a month to get Kw
+    let batterySize = parseInt(document.getElementById('battery-array-kwhs').innerHTML.replace('kWh', ''));
+
+    let pvOutput; //Dc array output hourly
+    let solarOutflow; //Flow out of PV
+    let amountStored; //Flow to battery
+    let gridOutflow; //Flow to grid
+    let batteryCapacity = batterySize + dcData[0] - averageConsumption; //Capacity of battery before current hour runs
+    let genChargeStart = 0.2; //What battery capactiy to start charging with generator at
+    let genSize = 10; //generator charging rate in kW
+    let genOutput;
+
+    //Initialize generator output
+    if (batteryCapacity/batterySize < genChargeStart) {
+        genOutput = genSize;
+    } else { genOutput = 0; }
+
+    let lastGeneratorDay;
+
+    //Iterate over all hours of dc solar array output
+    for (let i = 1; i < dcData.length; i++) {
+        pvOutput = dcData[i] * (solarArraySize / 1000); //Store solar array output data
+    
+        if (pvOutput - averageConsumption > 0) { 
+            solarOutflow = pvOutput - averageConsumption;
+        } else { 
+            solarOutflow = 0; 
+        }
+
+        if (solarOutflow + batteryCapacity > batterySize) {
+            amountStored = solarOutflow- (batteryCapacity + solarOutflow - batterySize);
+        } else {
+            amountStored = solarOutflow;
+        }
+
+        gridOutflow = solarOutflow - amountStored;
+
+        if (batteryCapacity + pvOutput + genOutput - averageConsumption) {
+            batteryCapacity = batteryCapacity + pvOutput + genOutput - averageConsumption
+        } else {
+            batteryCapacity = genSize;
+        }
+
+        //Generator was run
+        if (batteryCapacity/batterySize < genChargeStart) {
+            if (lastGeneratorDay == 'undefined') {
+                lastGeneratorDay = Math.floor(i/24);
+            }
+            genOutput = genSize;
+            console.log("GENERATOR RAN");
+            let currentDay = Math.floor(i/24);
+            if (currentDay != lastGeneratorDay) {
+                systemData['genDays'] = systemData['genDays'] + 1;
+                lastGeneratorDay = currentDay;
+            }
+        } else { 
+            genOutput = 0; 
+        }
+
+        systemData['pvOutput'] = systemData['pvOutput'] + pvOutput;
+        systemData['solarOutflow'] = systemData['solarOutflow'] + solarOutflow;
+        systemData['amountStored'] = systemData['amountStored'] + amountStored;
+        systemData['gridOutflow'] = systemData['gridOutflow'] + gridOutflow;
+        systemData['genOutput'] = systemData['genOutput'] + genOutput;
+    }
+    console.log(systemData);
+    return systemData;
+}
+
 //Function displays the electricity consumptions and sets up event listener for dropdown
 function handleUtilityResult(electricRate) {
     let monthlyConsumption = Math.round(billInp.value / electricRate);
@@ -103,9 +195,10 @@ function handleUtilityResult(electricRate) {
         console.log("Slider value is: " + slider.value);
         rateInp.value = parseInt(slider.value);
     });
-
+ 
     document.getElementById('utility-results').style.display = 'block';
     document.getElementById('module-system-information').style.display = 'block';
+    document.getElementById('module-fortress-system').style.display = 'block';
 
 }
  
